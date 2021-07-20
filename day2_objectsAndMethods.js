bb10d33caea79d8d812915163e19e0494a6bec9f
hopeless-soul <57869028+hopeless-soul@users.noreply.github.com>
2021-07-20 18:39:11 +0300

    Update day2_objectsAndMethods.js

--- day2_objectsAndMethods.js
+++ day2_objectsAndMethods.js
@@ -35,17 +35,28 @@ class Task {
         this.dueDate.setHours(this.dueDate.getHours() + hours);
     }
     postponeDay( days ) {
-        this.dueDate.setDate(this.dueDate.getDate() + 1);
+        this.dueDate.setDate(this.dueDate.getDate() + days);
     }
 }
 let yesterday = new Date(), tomorow = new Date();
 yesterday.setDate( yesterday.getDate() - 1  );
 tomorow.setDate( tomorow.getDate() + 1  );
 
+
+
 let task = new Task(0, false, "test1", yesterday, "Here the test desc.");
 let task2 = new Task(1, false, "test2", tomorow, "Here the test desc.");
-task.toogle();
+
+
 console.log("task1", task.isOverdue());
 console.log("task2", task2.isOverdue());
+
 task.toString();
 task2.toString();
+
+task.toogle();
+
+task.postponeDay(1);
+task.postponeHour(1);
+console.log("\ntask1", task.isOverdue());
+task.toString();
